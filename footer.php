2e14ec69e08a02ca062b65696112388f156ad207
iosefmann <mann@iosef.org>
2021-12-29 00:24:42 +1100

    Disable wordpress emoji functions, fixes footer logo due to webp conversion on build

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
 
 <footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<img id="footer_arrows" src='<?php bloginfo("stylesheet_directory"); ?>/inc/arrows.png'/>
+			<img width=30 height=30 id="footer_arrows" src='<?php bloginfo("stylesheet_directory"); ?>/inc/arrows.png'/>
 			<p>iosef.org © 2017 — <?php echo(date("Y")); ?> Joseph Mann</p>
 			<p><a href="<?php bloginfo('rss2_url')?>">RSS Feed</a></p>
 			<p class="footer_fineprint">By using iosef.org, you agree to the use of tracking cookies by third-parties to serve advertisements and to the <a href="https://iosef.org/terms.html">Terms of Use</a>, including the <a href="https://iosef.org/terms.html#privacy">privacy policy</a>. This site is protected by reCAPTCHA and the Google Privacy Policy and Terms of Service apply.</span>
--- functions.php
+++ functions.php
@@ -178,45 +178,58 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
-// Stop emoji script from loading
+// Stop emoji script from loading - Credit: Ryan Heller, GPL2, https://wordpress.org/plugins/disable-emojis/
 
-function disable_emoji_feature() {
-	
-	// Prevent Emoji from loading on the front-end
+/**
+ * Disable the emoji's
+ */
+function disable_emojis() {
 	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-	remove_action( 'wp_print_styles', 'print_emoji_styles' );
-
-	// Remove from admin area also
 	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-	remove_action( 'admin_print_styles', 'print_emoji_styles' );
-
-	// Remove from RSS feeds also
-	remove_filter( 'the_content_feed', 'wp_staticize_emoji');
-	remove_filter( 'comment_text_rss', 'wp_staticize_emoji');
-
-	// Remove from Embeds
-	remove_filter( 'embed_head', 'print_emoji_detection_script' );
-
-	// Remove from emails
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );	
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );	
 	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-
-	// Disable from TinyMCE editor. Currently disabled in block editor by default
 	add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
-
-	/** Finally, prevent character conversion too
-         ** without this, emojis still work 
-         ** if it is available on the user's device
-	 */
-
-	add_filter( 'option_use_smilies', '__return_false' );
-
+	add_filter( 'wp_resource_hints', 'disable_emojis_remove_dns_prefetch', 10, 2 );
 }
+add_action( 'init', 'disable_emojis' );
 
+/**
+ * Filter function used to remove the tinymce emoji plugin.
+ * 
+ * @param    array  $plugins  
+ * @return   array             Difference betwen the two arrays
+ */
 function disable_emojis_tinymce( $plugins ) {
-	if( is_array($plugins) ) {
-		$plugins = array_diff( $plugins, array( 'wpemoji' ) );
+	if ( is_array( $plugins ) ) {
+		return array_diff( $plugins, array( 'wpemoji' ) );
 	}
-	return $plugins;
+
+	return array();
 }
 
-add_action('init', 'disable_emoji_feature');
+/**
+ * Remove emoji CDN hostname from DNS prefetching hints.
+ *
+ * @param  array  $urls          URLs to print for resource hints.
+ * @param  string $relation_type The relation type the URLs are printed for.
+ * @return array                 Difference betwen the two arrays.
+ */
+function disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
+
+	if ( 'dns-prefetch' == $relation_type ) {
+
+		// Strip out any URLs referencing the WordPress.org emoji location
+		$emoji_svg_url_bit = 'https://s.w.org/images/core/emoji/';
+		foreach ( $urls as $key => $url ) {
+			if ( strpos( $url, $emoji_svg_url_bit ) !== false ) {
+				unset( $urls[$key] );
+			}
+		}
+
+	}
+
+	return $urls;
+}
